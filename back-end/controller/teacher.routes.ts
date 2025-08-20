aaaab5212fa163611da01f6cdf0682e58a157d2d
Zjepp <seppejanssens1505@gmail.com>
2025-08-20 13:40:10 +0200

    implement get all teachers in controller backend

--- back-end/controller/teacher.routes.ts
+++ back-end/controller/teacher.routes.ts
@@ -34,7 +34,14 @@ const teacherRouter = express.Router();
  *               items:
  *                  $ref: '#/components/schemas/Teacher'
  */
-teacherRouter.get('/', async (req: Request, res: Response, next: NextFunction) => {});
+teacherRouter.get('/', async (req: Request, res: Response, next: NextFunction) => { 
+    try {
+        const teachers = await teacherService.getAllTeachers();
+        res.json(teachers);
+    } catch (err) {
+        next(err);
+    }
+});
 
 /**
  * @swagger
