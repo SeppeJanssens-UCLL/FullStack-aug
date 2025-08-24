7c9a88e0be4b3d3dec84cf4924593df1038f065c
Zjepp <seppejanssens1505@gmail.com>
2025-08-24 16:30:53 +0200

    implement component to update learningpath frontend

--- front-end/components/learning-path/index.tsx
+++ front-end/components/learning-path/index.tsx
@@ -4,18 +4,30 @@ import { useState } from 'react';
 type Props = {
   teacherId: number;
   learningPath: string;
+  onChange: (teacherId: number, newPath: string) => void;
 };
 
-const LearningPath: React.FC<Props> = ({ teacherId, learningPath }: Props) => {
+const LearningPath: React.FC<Props> = ({ 
+  teacherId, 
+  learningPath, 
+  onChange 
+}: Props) => {
+  const [currentPath, setCurrentPath] = useState(learningPath);
+
   const handleLearningPathChange = (event: { target: { value: string } }) => {
-    {
-      /* Use TeacherService to update the learning path for the teacher */
-    }
+    const newPath = event.target.value;
+    setCurrentPath(newPath);
+    onChange(teacherId, newPath);
   };
 
   return (
     <div className="ml-6">
-      <select id="learningPath" className="ml-2 p-1" value={''}>
+      <select 
+        id="learningPath" 
+        className="ml-2 p-1" 
+        value={currentPath}
+        onChange={handleLearningPathChange}
+      >
         <option value="Infrastructure">Infrastructure</option>
         <option value="Software development">Software development</option>
         <option value="Cybersecurity">Cybersecurity</option>
