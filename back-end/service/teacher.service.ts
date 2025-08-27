9c3ff8764676e96d01b48670a5d30deeadb3b4a9
Zjepp <seppejanssens1505@gmail.com>
2025-08-27 13:00:43 +0200

    organise code a little

--- back-end/service/teacher.service.ts
+++ back-end/service/teacher.service.ts
@@ -3,7 +3,6 @@ import { Teacher } from '../model/teacher';
 
 const getAllTeachers = async (): Promise<Teacher[]> => teacherDb.getAllTeachers();
 
-const updateLearningPath = async (teacherId: number, learningPath: string): Promise<Teacher> =>
-    teacherDb.updateLearningPath(teacherId, learningPath);
+const updateLearningPath = async (teacherId: number, learningPath: string): Promise<Teacher> => teacherDb.updateLearningPath(teacherId, learningPath);
 
 export default { getAllTeachers, updateLearningPath };
