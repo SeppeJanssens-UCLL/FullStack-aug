4f39d4441ad47cb7eb98ead0a494116a2aeb2073
Zjepp <seppejanssens1505@gmail.com>
2025-08-27 13:05:18 +0200

    add classroom to header, only visible for logged in admin

--- front-end/components/header.tsx
+++ front-end/components/header.tsx
@@ -35,6 +35,13 @@ const Header: React.FC = () => {
           {t('header.nav.teachers')}
         </Link>
 
+        {loggedInUser && loggedInUser.role === 'admin' && (
+          <Link
+            href="/classroom"
+            className=" px-4 text-xl text-white hover:bg-gray-600 rounded-lg">
+            {t('header.nav.classrooms')}
+          </Link>)}
+
         {!loggedInUser && (
           <Link
             href="/login"
